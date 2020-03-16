a4ab7f65c8a7b586089bf886b0d196456c9d2db3
13020180013 <57305493+13020180013@users.noreply.github.com>
2020-03-16 23:32:47 +0800

    Add files via upload

---
+++ aulia ramadani/aulia.java
@@ -0,0 +1,23 @@
+/* NAMA  : Aulia Ramadani Hamzah
+   STB	 : 13020180013
+   KELAS : B1 TI
+   HARI/TGL : SENIN, 16 MARET 2020
+   WAKTU : 22.30 
+*/
+
+
+public class aulia {
+	
+	public static void main(String [] args) {
+	String nim ="13020180013";
+	String nama ="aulia ramadani hamzah";
+	String jurusan ="Teknik Informatika";
+	String fakultas ="Ilmu Komputer";
+	System.out.println (" nim : " + nim);
+	System.out.println (" nama : " + nama);
+	System.out.println (" jurusan : " + jurusan);
+	System.out.println (" fakultas : " + fakultas);
+
+	}
+   }
+	
